f426cb29db241355ca64e86b67d17fc19afeabc6
thangdtt <thangdtt2904@gmail.com>
2018-11-25 23:28:32 +0700

    Month calendar layout
    **Installation**
    npm install --save react-native-calendars

---
+++ MonthCalendar.js
@@ -0,0 +1,121 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Dimensions} from 'react-native';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      
+      <View>
+        <CalendarList
+        // Callback which gets executed when visible months change in scroll view. Default = undefined
+        onVisibleMonthsChange={(months) => {console.log('now these months are visible', months);}}
+        // Max amount of months allowed to scroll to the past. Default = 50
+        pastScrollRange={50}
+        // Max amount of months allowed to scroll to the future. Default = 50
+        futureScrollRange={50}
+        // Enable or disable scrolling of calendar list
+        scrollEnabled={true}
+        // Enable or disable vertical scroll indicator. Default = false
+        showScrollIndicator={false}
+        // Enable horizontal scrolling, default = false
+        horizontal={true}
+        // Enable paging on horizontal, default = false
+        pagingEnabled={true}
+        // Set custom calendarWidth.
+        calendarWidth={Dimensions.get('window').width}
+
+
+        //Calendar param
+
+        // Initially visible month. Default = Date()
+        // current={'2012-03-01'}
+        // // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+        // minDate={'2012-05-10'}
+        // // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+        // maxDate={'2012-05-30'}
+        // Handler which gets executed on day press. Default = undefined
+
+      
+        onDayPress={(day) => {console.log('selected day', day)}}
+        // Handler which gets executed on day long press. Default = undefined
+        onDayLongPress={(day) => {console.log('selected day', day)}}
+        // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+        monthFormat={'MMMM yyyy'}
+        // Handler which gets executed when visible month changes in calendar. Default = undefined
+        onMonthChange={(month) => {console.log('month changed', month)}}
+        // Hide month navigation arrows. Default = false
+        hideArrows={false}
+        // Replace default arrows with custom ones (direction can be 'left' or 'right')
+        renderArrow={(direction) => (<Arrow />)}
+        // Do not show days of other months in month page. Default = false
+        hideExtraDays={false}
+        // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
+        // day from another month that is visible in calendar page. Default = false
+        disableMonthChange={false}
+        // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
+        firstDay={1}
+        // Hide day names. Default = false
+        hideDayNames={false}
+        // Show week numbers to the left. Default = false
+        showWeekNumbers={false}
+        // Handler which gets executed when press arrow icon left. It receive a callback can go back month
+        onPressArrowLeft={substractMonth => substractMonth()}
+        // Handler which gets executed when press arrow icon left. It receive a callback can go next month
+        onPressArrowRight={addMonth => addMonth()}
+
+        style={{
+          borderWidth: 2,
+          borderColor: '#a4a4a4',
+          height: 320
+        }}
+        // Specify theme properties to override specific styles for calendar parts. Default = {}
+        theme={{
+          backgroundColor: '#000000',
+          calendarBackground: '#ffffff',
+          textSectionTitleColor: '#b6c1cd',
+          selectedDayBackgroundColor: '#00adf5',
+          selectedDayTextColor: '#ffffff',
+          todayTextColor: '#00adf5',
+          dayTextColor: '#2d4150',
+          textDisabledColor: '#d9e1e8',
+          dotColor: '#00adf5',
+          selectedDotColor: '#ffffff',
+          arrowColor: '#2d4150',
+          monthTextColor: '#2d4150',
+          textDayFontFamily: 'roboto',
+          textMonthFontFamily: 'roboto',
+          textDayHeaderFontFamily: 'roboto',
+          textMonthFontWeight: 'bold',
+          textDayFontSize: 16,
+          textMonthFontSize: 16,
+          textDayHeaderFontSize: 16
+        }}
+        
+        >
+        </CalendarList>
+      </View>
+      
+       
+    );
+    
+  }
+}
+
+
